40ebf6b13bfb371bd2bb9faf3925b466f1d16363
brother2000years <non@email.ru>
2015-11-01 13:06:09 +0300

    Added an example of using Logger - Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LoggerEvsSpace;
+namespace LoggerEv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoggerEvs.messageCame += testToConsole;
+            
+            for (int i = 0; i < 10; i++ )
+                LoggerEvs.writeLog("Test note №" + i.ToString());
+            
+            Console.ReadKey();
+        }
+
+        public static void testToConsole(String message)
+        {
+            Console.Write(message);
+        }
+    }
+}
